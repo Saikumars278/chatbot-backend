e9c7c72a2fe62fbfde8f6ce5696301c1fac795cb
Saikumar S <saikumar2780@gmail.com>
2026-08-20 11:11:17 +0530

    Add requirements.txt, Procfile, and Render deployment settings

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -49,6 +49,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
@@ -58,6 +59,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://*.onrender.com",
 ]
 
 ROOT_URLCONF = 'chatproject.urls'
@@ -136,6 +138,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
